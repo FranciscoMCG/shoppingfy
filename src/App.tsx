5a28630bcac8fd904a38383d828d528c6ca4958b
franciscogomestv@gmail.com <franciscogomestv@gmail.com>
2021-01-27 08:26:34 +0000

    redux initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = () => {
+  return <AppContainer>Shopping</AppContainer>;
+};
 
 export default App;
