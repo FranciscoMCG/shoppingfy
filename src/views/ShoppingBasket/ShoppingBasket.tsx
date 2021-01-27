940455a0d6fdf793e5197557e6f9295ee0d2e94d
franciscogomestv@gmail.com <franciscogomestv@gmail.com>
2021-01-27 08:29:05 +0000

    creted the shopping basket scaffold

---
+++ src/views/ShoppingBasket/ShoppingBasket.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const ShoppingBasketContainer = styled.div``;
+const ShoppingBasket = () => {
+  return <ShoppingBasketContainer>Shopping</ShoppingBasketContainer>;
+};
+
+export default ShoppingBasket;
