39699d17ab4b4b33db357a022fc9e2fdbb58d5ac
franciscogomestv@gmail.com <franciscogomestv@gmail.com>
2021-01-27 08:32:02 +0000

    created colors obj, added baseUrl to tsconfig

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export default {
+  white: '#FFF',
+  black: '#000',
+  greyLight: '#D3D3D3',
+  greyDark: '#A9A9A9',
+  blueLight: '#46B7FD',
+  orange: '#FFA500',
+};
--- src/views/ShoppingBasket/ShoppingBasket.tsx
+++ src/views/ShoppingBasket/ShoppingBasket.tsx
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
-const ShoppingBasketContainer = styled.div``;
+import colors from 'styles/colors';
+
+const ShoppingBasketContainer = styled.div`
+  border: 2px solid ${colors.greyLight};
+  max-width: 30rem;
+  width: 70%;
+  padding: 2rem 1.2rem;
+`;
 const ShoppingBasket = () => {
   return <ShoppingBasketContainer>Shopping</ShoppingBasketContainer>;
 };
